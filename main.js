36fc17050769fd0dc6ec24ce71a4bbb4e0e149ef
SeizeDub <cesardubuisson@msn.com>
2020-11-30 19:10:05 +0100

    working todolist

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const taskButton = document.getElementById('task-button');
+const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
+
 
 // functions
 
 function createTask(taskContent) {
     // crer un li
     let task = document.createElement('li');
-    // mettre les données dans le li
+    // ajouter le texte dans le li
     task.textContent = taskContent;
+    // creer un bouton
+    let deleteButton = document.createElement('button');
+    //ajouter la classe au bouton
+    deleteButton.classList.add('delete-button')
+    //ajouter le texte du bouton
+    deleteButton.textContent = 'X';
+    //ajouter le bouton dans le li
+    task.appendChild(deleteButton);
     // mettre le li dans le ul
     taskList.appendChild(task);
+    //vider le formulaire
+    taskInput.value = null;
+}
+
+function deleteTask(task) {
+    task.remove();
 }
 
 //event listeners
-taskButton.onclick = () => {
-    let taskContent = prompt('Nouvelle tache');
-    createTask(taskContent);
+document.onclick = (event) => {
+    if (event.target.classList.contains('delete-button')) deleteTask(event.target.parentNode);
+}
+
+document.onsubmit = (event) => {
+    event.preventDefault();
+    if (taskInput.value) createTask(taskInput.value);
+    
 }
